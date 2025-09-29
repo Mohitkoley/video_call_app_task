91fde4d68dd24fae11e80310013293c2cee49628
Mohit koley <mohitkoley5@gmail.com>
2025-09-29 14:01:05 +0530

    Renamed android and ios

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.video_calling"
+    namespace = "com.dummy.video_call"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.video_calling"
+        applicationId = "com.dummy.video_call"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
